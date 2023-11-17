6fab42e82d8798fb488fedb4ad1fbab974587a0f
grinono <bartel_verkruijssen@hotmail.com>
2023-11-17 15:32:22 +0100

    add files

---
+++ build.sh
@@ -0,0 +1,7 @@
+# rm -rf anon 
+# rm -rf cloundnative
+
+git clone https://gitlab.com/dalibo/postgresql_anonymizer.git anon
+git clone https://github.com/cloudnative-pg/postgis-containers.git postgis
+
+# cp -rf postgis/PostGis/16 ./custom
